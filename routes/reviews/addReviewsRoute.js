346e87b041561c94100eb60653d2e418b64049b1
agussaputran <ntoiagussaputra@gmail.com>
2020-11-16 22:17:21 +0700

    [FIXING]

--- routes/reviews/addReviewsRoute.js
+++ routes/reviews/addReviewsRoute.js
@@ -11,18 +11,15 @@ app.post("/reviews/:movie_id", async (req, res, next) => {
   let userId = (body.userId = req.user.id);
   let movieId = (body.movie_id = req.params.movie_id);
 
-  const isMovieIdExist = await db.get("reviews", { movieId }).catch((err) => {
-    next(err);
-  });
-  const isUserIdExist = await db.get("reviews", { userId }).catch((err) => {
-    next(err);
-  });
+  const isMovieIdExist = await db
+    .get("reviews", { movieId, userId })
+    .catch((err) => {
+      next(err);
+    });
 
-  if (isUserIdExist && isUserIdExist.length) {
-    if (isMovieIdExist && isMovieIdExist.length) {
-      res.status(409).send("you only can add one reviews");
-      return;
-    }
+  if (isMovieIdExist && isMovieIdExist.length) {
+    res.status(409).send("you only can add one reviews");
+    return;
   }
 
   const result = await db.add("reviews", body).catch((err) => {
