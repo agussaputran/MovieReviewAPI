ecd08258b5fc8accc47b8d7f7f2e57a4b94c9960
Din Fikri <fikridin22@gmail.com>
2020-11-17 04:50:49 +0700

    add email filter and token in register

--- routes/authAdmins/registerRoute.js
+++ routes/authAdmins/registerRoute.js
@@ -3,6 +3,8 @@ const app = express.Router()
 const db = require('../../controller/authOrCanUseGlobal/dbController')
 const { salt } = require('../../helper/bcryptHelper')
 const routeErrorHandler = require('../../middleware/errorMiddleware')
+const jwt = require('jsonwebtoken')
+const secret = process.env.JWT_SECRET
 
 app.post('/authAdmins/register', async (req, res, next) => {
   const username = req.body.username
@@ -15,6 +17,13 @@ app.post('/authAdmins/register', async (req, res, next) => {
     })
   if (isUsernameExist && isUsernameExist.length) {
     return res.status(409).send('The same username has exist')
+  }
+  const isEmailExist = await db.get('userAdmins', { email })
+    .catch(err => {
+      next(err)
+    })
+  if (isEmailExist && isEmailExist.length) {
+    return res.status(409).send('Email already used')
   }
   const hashedPassword = await salt(password)
     .catch(err => {
@@ -30,6 +39,10 @@ app.post('/authAdmins/register', async (req, res, next) => {
       next(err)
     })
   if (addUserResult) {
+    const token = jwt.sign(addUserResult, secret, {
+      expiresIn: '6h'
+    })
+    addUserResult.token = token
     res.send(addUserResult)
   } else {
     res.status(400).send('Wrong body')
