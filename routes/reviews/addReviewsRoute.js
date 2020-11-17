965d4bd787769d8a30882c1954f1c887a4285ac0
agussaputran <ntoiagussaputra@gmail.com>
2020-11-18 02:12:19 +0700

    [ADD] - insert now date helper

--- routes/reviews/addReviewsRoute.js
+++ routes/reviews/addReviewsRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express.Router();
 const db = require("../../controller/authOrCanUseGlobal/dbController");
+const inserNowDate = require("../../helper/inserDateHelper");
 const passport = require("../../middleware/authorizationMiddleware");
 const errorMiddleware = require("../../middleware/errorMiddleware");
 
@@ -10,13 +11,7 @@ app.post("/reviews/:movie_id", async (req, res, next) => {
   let body = req.body;
   let userId = (body.userId = req.user.id);
   let movieId = (body.movie_id = req.params.movie_id);
-
-  let today = new Date();
-  let date =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-  let time =
-    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  body.date = date + " " + time;
+  body.date = inserNowDate();
 
   const isReviewed = await db
     .get("reviews", { movieId, userId })
