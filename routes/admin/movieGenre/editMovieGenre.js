9b2bbb811bc01017389ad41ed07fb97ded23f27f
Din Fikri <fikridin22@gmail.com>
2020-11-18 10:48:28 +0700

    innit commit

---
+++ routes/admin/movieGenre/editMovieGenre.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express.Router()
+const db = require('../../../controller/movieGenres/dbController')
+const passport = require('../../../middleware/authorizationMiddleware')
+const routeErrorHandler = require('../../../middleware/errorMiddleware')
+
+app.use(passport.authenticate('bearer', { session: false }))
+
+app.patch('/movieGenres', async (req, res, next) => {
+    const id = req.body.id
+    const body = req.body
+
+    const editMovieGenre = await db.edit('movieGenre', id, body)
+        .catch(err => {
+            next(err)
+        })
+    if (editMovieGenre) {
+        res.send(editMovieGenre)
+    }
+})
+
+app.use(routeErrorHandler)
+
+module.exports = app
