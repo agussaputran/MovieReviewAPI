0d9dfa98f82f2f0ca5899dc3ea117e47b969673d
Din Fikri <fikridin22@gmail.com>
2020-11-18 18:07:50 +0700

    test ssh

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express.Router()
 const db = require('../controller/authOrCanUseGlobal/dbController')
 
+
 app.get('/', (req, res) => {
   res.send("Hello world!")
 })
